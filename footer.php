990e33e1360559335df8973406c9123d496a2bb0
Jake Yu <dev@eightydigital.com>
2014-05-06 10:18:03 +0800

    Initial files

---
+++ footer.php
@@ -0,0 +1,18 @@
+
+	<footer id="main-footer">
+		<?php get_sidebar( 'footer' ); ?>
+
+		<div id="footer-bottom">
+			<div class="container clearfix">
+				<center id="copyright">
+				&copy; 2014. All rights reserved. <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Paper Carpenter</a>
+				</center>
+			</div>	<!-- .container -->
+		</div>
+	</footer> <!-- #main-footer -->
+
+
+
+	<?php wp_footer(); ?>
+</body>
+</html>
