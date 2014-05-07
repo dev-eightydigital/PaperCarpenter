f1d226063b8fef5e9dd15576262ba6bd91991f5e
Jake Yu <dev@eightydigital.com>
2014-05-07 15:03:32 +0800

    deleted unnecessary functions

--- functions.php
+++ functions.php
@@ -1,11 +1,4 @@
 <?php
-
-function my_page_menu_args($args = '') {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter('wp_page_menu_args', 'my_page_menu_args');
-
 //***************footer logo widget**********************
 class footlogo_widget extends WP_Widget {
 
