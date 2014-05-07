fd382f0c89a59eb0be902a0f8e02dd05d17fd891
Jake Yu <dev@eightydigital.com>
2014-05-07 15:05:00 +0800

    copy other codes to fullscreen homepage

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-<?php if ( ! isset( $_SESSION ) ) session_start(); ?>
+<?php 
+define('FULLSCREEN_HOMEPAGE', 763);
+if ( ! isset( $_SESSION ) ) session_start(); ?>
 <!DOCTYPE html>
 <!--[if IE 6]>
 <html id="ie6" <?php language_attributes(); ?>>
@@ -27,7 +29,37 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( $template_directory_uri . '/js/html5.js"' ); ?>" type="text/javascript"></script>
 	<![endif]-->
-
+	
+	<?php if( is_page(FULLSCREEN_HOMEPAGE) ): //page id of homepage set for the fullscreen background ?>
+		<link rel="stylesheet" href="<?= $template_directory_uri?>/fullscreen-style.css" media="all" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+		<?php $plax_path = $template_directory_uri.'/js/plax/plax.js';?>
+		<?php echo '<script type="text/javascript" src="'. $plax_path .'"></script>'; ?>
+		<style type="text/css">
+		  * {
+			margin: 0px;
+			padding: 0px;
+		  }
+		  body {
+			position: relative;
+		  }
+		  div#shell{
+			z-index: 1;
+		  }
+		  img#plax-logo {
+			position: relative;
+			top: -10px;
+			left: 0px;
+			z-index: 3;
+		  }
+		</style>
+		<script type="text/javascript">
+		  $(document).ready(function () {
+			$('#shell img').plaxify()
+			$.plax.enable()
+		  })
+		</script>
+	<?php endif;?>
 	<script type="text/javascript">
 		document.documentElement.className = 'js';
 	</script>
@@ -37,66 +69,68 @@
 <body <?php body_class(); ?>>
 	<header id="main-header">
 		<div class="container clearfix">
-		<?php
-			$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && '' != $user_logo
-				? $user_logo
-				: $template_directory_uri . '/images/logo.png';
-		?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" />
-			</a>
-			<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-			<div id="et-top-navigation">
-				<nav id="top-menu-nav">
+			<div id="shell">
 				<?php
-					$menuClass = 'nav';
-					if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
-					$primaryNav = '';
-
-					$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
-
-					if ( '' == $primaryNav ) :
+					$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && '' != $user_logo
+						? $user_logo
+						: $template_directory_uri . '/images/logo.png';
 				?>
-				<?php wp_nav_menu('Paper Carpenter'); ?>
-					<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
-						<?php //if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
-							<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
-						<?php //}; ?>
-
-						<?php show_page_menu( $menuClass, false, false ); ?>
-						<?php show_categories_menu( $menuClass, false ); ?>
-					</ul>
-				<?php
-					else :
-						echo( $primaryNav );
-					endif;
-				?>
-				</nav>
-				
-				<div id="et_top_search">
-					<span id="et_search_icon"></span>
-					<form role="search" method="get" class="et-search-form et-hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<?php
-						printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
-							esc_attr_x( 'Search &hellip;', 'placeholder', 'Divi' ),
-							get_search_query(),
-							esc_attr_x( 'Search for:', 'label', 'Divi' )
-						);
-					?>
-					</form>
-				</div>
-				<div class="social-icons">
-				<ul>
-				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-				<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-				</ul>
-				</div>
-
-				<?php do_action( 'et_header_top' ); ?>
-				<div class="clear"></div>
-			</div> <!-- #et-top-navigation -->
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" <?php if( is_page(FULLSCREEN_HOMEPAGE) ){ echo 'data-xrange="20" data-yrange="20" id="plax-logo"';} else { echo 'id="logo"';}?>/>
+					</a>
+					<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+					<div id="et-top-navigation">
+						<nav id="top-menu-nav">
+						<?php
+							$menuClass = 'nav';
+							if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
+							$primaryNav = '';
+		
+							$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
+		
+							if ( '' == $primaryNav ) :
+						?>
+						<?php wp_nav_menu('Paper Carpenter'); ?>
+							<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
+								<?php //if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
+									<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
+								<?php //}; ?>
+		
+								<?php show_page_menu( $menuClass, false, false ); ?>
+								<?php show_categories_menu( $menuClass, false ); ?>
+							</ul>
+						<?php
+							else :
+								echo( $primaryNav );
+							endif;
+						?>
+						</nav>
+						
+						<div id="et_top_search">
+							<span id="et_search_icon"></span>
+							<form role="search" method="get" class="et-search-form et-hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<?php
+								printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
+									esc_attr_x( 'Search &hellip;', 'placeholder', 'Divi' ),
+									get_search_query(),
+									esc_attr_x( 'Search for:', 'label', 'Divi' )
+								);
+							?>
+							</form>
+						</div>
+						<div class="social-icons">
+						<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+						</ul>
+						</div>
+		
+						<?php do_action( 'et_header_top' ); ?>
+						<div class="clear"></div>
+					</div> <!-- #et-top-navigation -->
+			</div>
 		</div> <!-- .container -->
 	</header> <!-- #main-header -->
 	
