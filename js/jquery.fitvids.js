85c65b22be9c99de49c31890a9af76ee048d55d7
Jake Yu <dev@eightydigital.com>
2014-05-15 12:41:23 +0800

    return default video js

--- js/jquery.fitvids.js
+++ js/jquery.fitvids.js
@@ -80,13 +80,8 @@
 		  var videoID = 'fitvid' + Math.floor(Math.random()*999999);
 		  $this.attr('id', videoID);
 		}
-		$this.wrap('<center class="fluid-width-video-wrapper"></center>').parent('.fluid-width-video-wrapper')
-			.css({
-				'padding-top': $this.attr('height')+"px",
-				'width' : $this.attr('width'),
-				'margin': '0 auto'
-				});//(aspectRatio * 90)+"%");
-		//$this.removeAttr('height').removeAttr('width');
+		$this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+		$this.removeAttr('height').removeAttr('width');
 	  });
 	});
   };
