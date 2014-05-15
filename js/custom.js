2c81ae7424a501d9839f56b9b8f7cd0baf7c4bdc
Jake Yu <dev@eightydigital.com>
2014-05-15 11:41:40 +0800

    clean js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 (function($){
-	//$('.et_pb_slider').append('<img src="'+ templateUrl +'/images/slide_arrow_down.gif" alt="" class="slider_arrow_down" />');
 	$(window).resize(function (){
 		if($(window).width() < 950){
 			$('#fsi-full-bg img:first-child').attr('src', templateUrl+'/images/small_res_img.jpg');
